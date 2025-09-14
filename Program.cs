3aae07afa30fb50c37958d3fe376b1ead846ab02
Bar Yaakov <baryaakov5050@gmail.com>
2025-09-14 11:53:30 -0400

    added the array of animals

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string[] pettingZoo = {
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
